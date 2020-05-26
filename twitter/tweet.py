343df146b174f58088303f04aa4987bef62c9fa6
Eduardo <eriveros@dcc.uchile.cl>
2020-05-26 15:47:17 -0400

    fixing marker in tweet

--- twitter/tweet.py
+++ twitter/tweet.py
@@ -2,6 +2,7 @@ import asyncio
 
 from core.abstract_item import AbstractItem
 
+
 class Tweet(AbstractItem):
     def __init__(self, id: int, date: str, author: str, message: str):
         self.id = id
@@ -11,16 +12,7 @@ class Tweet(AbstractItem):
         super().__init__()
 
     def get_raw_data(self) -> bytes:
-        return (str(self.id) + self.date + self.author + self.message).decode()
+        return (self.date + str(self.id) + self.author + self.message).encode()
 
     def get_marker(self) -> str:
-        return id
-
-    async def read(self, reader: asyncio.StreamReader):
-        await self.header.read(reader)
-        to_read = self.header.body_size
-        while to_read != 0:
-            new_data = await reader.read(to_read)
-            to_read -= len(new_data)
-            self.data += new_data
-
+        return self.date
